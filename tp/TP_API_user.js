5b5e2c7dfd2f372f22ca2a3801bbba0f25a253c7
xsium <simon.metayerlr@gmail.com>
2024-10-21 14:03:03 +0200

    add TP API random User

---
+++ tp/TP_API_user.js
@@ -0,0 +1,64 @@
+let url = "https://randomuser.me/api/";
+let divApi = document.querySelector("#apiUser");
+
+let newcard = document.createElement("div");
+newcard.classList.add("card", "mx-auto", "m-3");
+newcard.style.width = "350px";
+newcard.style.backgroundColor = "#454545";
+newcard.style.margin = "5px";
+newcard.style.textAlign = "center";
+
+let newIMG = document.createElement("img");
+newIMG.classList.add("card-header");
+
+let newDiv = document.createElement("div");
+newDiv.classList.add("card-body");
+newDiv.style.color = "white";
+newDiv.style.borderRadius = "10px";
+
+let pName = document.createElement("p");
+pName.classList.add("card-body");
+pName.style.color = "white";
+pName.style.borderRadius = "10px";
+
+let btnRandom = document.createElement("div");
+btnRandom.classList.add("btn", "btn-secondary");
+btnRandom.style.color = "white";
+btnRandom.addEventListener("click", loadUser);
+btnRandom.innerText = "get random";
+
+divApi.append(newcard);
+newcard.append(newIMG);
+newcard.append(newDiv);
+newDiv.append(pName);
+newcard.append(btnRandom);
+
+async function loadUser() {
+  const dataUser = await getUser(url);
+  displayUser(dataUser);
+}
+
+async function getUser(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!response.ok) {
+    const error = (data && data.message) || response.statusText;
+    return Promise.reject(error);
+  }
+  return data.results[0];
+}
+
+function displayUser(toPrint) {
+  newcard.id = toPrint.name.title;
+  newIMG.src = toPrint.picture.medium;
+  pName.innerText = `${toPrint.name.title}. ${toPrint.name.last} ${toPrint.name.first}
+  
+  ${toPrint.email}
+
+  Adress: ${toPrint.location.street.number}- ${toPrint.location.street.name}
+  ${toPrint.location.city} - ${toPrint.location.country}
+
+  ${toPrint.cell}
+  `;
+}
+loadUser();
